bb2de77cf1138a5814ad54fb3e8a8d5ff3cb22e4
Echis65 <Evansatompoya@gmail.com>
2023-01-12 04:14:15 +0000

    Added a header componetn

---
+++ src/components/Header.vue
@@ -0,0 +1,17 @@
+<template>
+  <header>
+    <div class="text-center bg-metal p-5 mb-10 text-white">
+      <div class="text-3xl md:text-5xl mb-4"><span class="material-symbols-outlined mr-2 text-3xl">
+        coronavirus
+        </span>Covid-19 Tracker</div>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+}
+</script>
+
+
