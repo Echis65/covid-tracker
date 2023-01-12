500c9bbc7ae41b9bf0ed9cc9e79c9e52e806b6e6
Echis65 <Evansatompoya@gmail.com>
2023-01-12 04:13:48 +0000

    title component for showing current country and date

---
+++ src/components/DataTitle.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="text-center ">
+        <div class="text-3xl">{{text}}</div>
+        <div class="text-2xl mt-4 mb-10  ">{{timeStamp}}</div>
+    </div>
+</template>
+
+
+<script>
+import moment from 'moment'
+export default {
+    props:['text', 'dataDate'],
+    computed: {
+        timeStamp() {
+         return moment(this.dataDate).format('Do MMMM Y')
+        }
+    }
+}
+</script>
