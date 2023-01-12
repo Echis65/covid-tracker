7037c0ab3450657670dd043049361a532263b048
Echis65 <Evansatompoya@gmail.com>
2023-01-12 04:13:10 +0000

    Country select component for selecting a list of countries

---
+++ src/components/CountrySelect.vue
@@ -0,0 +1,14 @@
+<template>
+    <select class="form-select w-full p-3 mt-10 rounded block border">
+        <option value="0">
+
+        </option>
+    </select>
+</template>
+
+<script>
+export default {
+    name: 'CountrySelect',
+    props: ['countries']
+}
+</script>
