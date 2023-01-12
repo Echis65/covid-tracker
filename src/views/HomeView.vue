ba1d79c4f183a432aad9cab0d42be6faa9a99128
Echis65 <Evansatompoya@gmail.com>
2023-01-12 04:14:46 +0000

    Added various components need on the homepage

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,58 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+ <main v-if="!loading">
+    <DataTitle :text="title" :dataDate="dataDate"/>
+    <DataCards :newConfirmed="stats.NewConfirmed" :totalConfirmed="stats.TotalConfirmed" :newDeaths="stats.NewDeaths" :totalDeaths="stats.TotalDeaths"/>
+    <CountrySelect :countries="countries"/>
+ </main>
+
+ <main v-else class="flex align-center justify-center flex-col">
+    <div class="text-gray-500 text-center mt-10 text-3xl">
+      Fetching Data... 
+    </div>
+    <div >
+      <iframe  src="https://giphy.com/embed/jAYUbVXgESSti" width="250" height="200" frameBorder="0" class="giphy-embed m-auto" allowFullScreen></iframe>
+    </div>
+ </main>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import axios from 'axios'
+import DataTitle from "@/components/DataTitle.vue"
+import DataCards from '@/components/DataCards.vue';
+import CountrySelect from '@/components/CountrySelect.vue';
 
 export default {
-  name: 'HomeView',
+  name: "HomeView",
   components: {
-    HelloWorld
-  }
-}
+    DataTitle,
+    DataCards,
+    CountrySelect,
+  },
+  data(){
+    return {
+      loading: true,
+      title: 'Global',
+      dataDate: '',
+      countries: [],
+      stats: {}
+    }
+  },
+  methods: {
+    async fetchCovidData () {
+      const res = await axios.get("https://api.covid19api.com/summary")
+      if(res.status === 200){
+        return res.data
+      }
+    }
+  },
+  async created() {
+      const data =  await this.fetchCovidData()
+      console.log(data)
+      this.dataDate = data.Date
+      this.stats = data.Global
+      this.countries = data.Countries
+      this.loading = false
+  } 
+};
 </script>
